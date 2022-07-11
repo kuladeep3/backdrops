324160238d6a67469d3a43ffd7264eb23dd1bfdb
Kuladeep <kuladeepu1@gmail.com>
2022-07-11 10:39:16 +0530

    download icon change

--- components/Backdrops.js
+++ components/Backdrops.js
@@ -19,8 +19,15 @@ const Backdrops = ({ data, config }) => {
           return (
             <div key={backdrop.file_path} className='wall'>
               <Image src={`${config.base_url}original${backdrop.file_path}`} width={backdrop.width} height={backdrop.height} layout='responsive' alt='wallpaper' />
-              <Button variant='contained' size='small' href={`${config.base_url}original${backdrop.file_path}`} target='_blank'>
-                Download
+              <Button variant='text' size='small' href={`${config.base_url}original${backdrop.file_path}`} download>
+                <svg xmlns='http://www.w3.org/2000/svg' enableBbackground='new 0 0 24 24' height='36px' viewBox='0 0 24 24' width='36px' fill='#fff'>
+                  <g>
+                    <rect fill='none' height='24' width='24' />
+                  </g>
+                  <g>
+                    <path d='M18,15v3H6v-3H4v3c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-3H18z M17,11l-1.41-1.41L13,12.17V4h-2v8.17L8.41,9.59L7,11l5,5 L17,11z' />
+                  </g>
+                </svg>
               </Button>
             </div>
           );
